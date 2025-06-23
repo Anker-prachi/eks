e1aeb038bbe96d6951eb2fcf93be2d18a198c479
Anker-prachi <prachi.tiwari@ankercloud.com>
2025-06-23 15:40:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // index.js
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
-// BAD: Using eval() with user input - triggers CodeQL RCE alert
+// BAD: using user input directly in eval
 http.createServer((req, res) => {
-    const queryObject = url.parse(req.url, true).query;
-    const result = eval(queryObject.input); // Vulnerability here
-    res.end(`Result is: ${result}`);
+  const query = url.parse(req.url, true).query;
+  const result = eval(query.code); // <-- Trigger RCE vulnerability
+  res.end(`Result: ${result}`);
 }).listen(8080);
+
+// BAD: reading file path from user input
+const userFile = process.argv[2];
+fs.readFile(userFile, (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
